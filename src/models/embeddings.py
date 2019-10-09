2a6f17f83b29855fe5a142b464955ab89aab630d
Elvan Aydemir <elvan@amplifyanalytix.com>
2019-10-09 17:35:26 +0200

    Initialize

---
+++ src/models/embeddings.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+import tensorflow_datasets as tfds
+tfds.disable_progress_bar()
